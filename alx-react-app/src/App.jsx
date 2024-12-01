ce7f7e0e616d6797ff93f9e26a1c29d1028fee8b
Fidelis Ifediora <fidelisemekaifediora@gmail.com>
2024-12-01 11:35:33 -0800

    alx react app project

---
+++ alx-react-app/src/App.jsx
@@ -0,0 +1,33 @@
+
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import WelcomeMessage from './WelcomeMessage'
+import Header from './assets/Header'
+import MainContent from './MainContent'
+import UserProfile from './UserProfile'
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <div>
+        <WelcomeMessage/>
+        <Header/>
+        <MainContent/>
+        <UserProfile ame="Alice" age="25" bio="Loves hiking and photography"/>
+
+      </div>
+    </>
+  )
+}
+
+export default App
