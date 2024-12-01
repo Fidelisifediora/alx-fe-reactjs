e67d6d58b8209a0febcb4de111905e4f1c1f68f4
Fidelis Ifediora <fidelisemekaifediora@gmail.com>
2024-12-01 15:11:37 -0800

    Add react-router-dom project

---
+++ my-company/src/App.jsx
@@ -0,0 +1,37 @@
+
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './Home'
+import About from './About'
+import Navbar from './Navbar'
+import Services from './Services'
+import Contact from './Contact'
+function App() {
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <div>
+      <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+      </div>
+    </>
+  )
+}
+
+export default App
