a638a3ae3e6daf72c46a8ffa58a029d2b4536b6a
Fidelis Ifediora <fidelisemekaifediora@gmail.com>
2024-12-08 04:55:05 -0800

    Update App.jsx

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,19 +1,12 @@
-
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import UserProfile from './UserProfile'
+import React from "react";
+import UserProfile from "./components/UserProfile";
 
 function App() {
-  
   return (
-    <>
-      <div>
-      <UserProfile/>
-      </div>
-      
-    </>
-  )
+    <div className="App">
+      <UserProfile />
+    </div>
+  );
 }
 
-export default App
+export default App;
