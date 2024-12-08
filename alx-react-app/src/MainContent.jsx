b71a5a30e481aee39b6173cf659fc92d421e24bc
Fidelis Ifediora <fidelisemekaifediora@gmail.com>
2024-12-08 14:15:33 -0800

    Delete alx-react-app directory

--- alx-react-app/src/MainContent.jsx
+++
@@ -1,8 +0,0 @@
-function MainContent (){
-    return(
-        <main>
-        <p style={{fontweight:'bold', textAlign:'center'}}>I love to visit New York, Paris, and Tokyo.</p>
-      </main> 
-    )
-}
-export default MainContent
