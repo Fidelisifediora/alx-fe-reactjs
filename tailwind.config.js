7b77803f07738ca3565233bf359cd48cd69e5dc3
Fidelis Ifediora <fidelisemekaifediora@gmail.com>
2024-12-08 07:05:36 -0800

    Add react tailwind.css project

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false,  // You can enable dark mode if required
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+
