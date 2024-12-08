0d6eebe001bca87f59b511ce9e4c91fd2d99e7d2
Fidelis Ifediora <fidelisemekaifediora@gmail.com>
2024-12-07 23:36:39 -0800

    Add tailwind styling

---
+++ tailwind-react-integration/src/App.jsx
@@ -0,0 +1,19 @@
+
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import UserProfile from './UserProfile'
+
+function App() {
+  
+  return (
+    <>
+      <div>
+      <UserProfile/>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
