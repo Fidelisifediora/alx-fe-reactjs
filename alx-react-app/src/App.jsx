cfa65d45ea5543b38e97bb3576717fb47f65fbb2
Fidelis Ifediora <fidelisemekaifediora@gmail.com>
2024-12-09 04:52:57 +0000

    Update App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,12 +1,12 @@
 
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import reactLogo from './components/assets/react.svg'
+import viteLogo from '/components/vite.svg'
 import './App.css'
-import WelcomeMessage from './WelcomeMessage'
-import Header from './assets/Header'
-import MainContent from './MainContent'
-import Footer from './Footer'
-import UserProfile from './UserProfile'
+import WelcomeMessage from './components/WelcomeMessage'
+import Header from './components/Header'
+import MainContent from './components/MainContent'
+import Footer from './components/Footer'
+import UserProfile from './components/UserProfile'
 
 function App() {
 
