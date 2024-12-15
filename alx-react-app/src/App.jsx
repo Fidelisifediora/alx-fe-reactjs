3955281a0d8381065a4273a5e4cc8f901f4fdf0a
Fidelis Ifediora <fidelisemekaifediora@gmail.com>
2024-12-15 10:13:37 -0800

    Add react component structure example

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,10 +1,15 @@
-import { useState } from 'react'
+
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './Header'
+import MainContent from '../MainContent'
+import Footer from './Footer'
+import WelcomeMessage from './WelcomeMessage'
+import UserProfile from './UserProfile'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
@@ -16,18 +21,14 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <WelcomeMessage/>
+      <Header/>
+      <MainContent/>
+      <Footer/>
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+
+    </div>
     </>
   )
 }
