27b6c851a0877602235510f923b3a7018400adb2
Fidelis Ifediora <fidelisemekaifediora@gmail.com>
2024-12-23 22:19:21 +0000

    Update App.jsx

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,36 +1,13 @@
+import React from "react";
+import Search from "./components/Search";
 
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Header from './Header'
-import MainContent from '../MainContent'
-import Footer from './Footer'
-import WelcomeMessage from './WelcomeMessage'
-import UserProfile from './UserProfile'
-import Counter from './Counter'
-function App() {
-
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-    <div>
-      <WelcomeMessage/>
-      <Header/>
-      <MainContent/>
-      <Footer/>
-      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
-      <Counter/>
+    <div className="App">
+      <h1 className="text-center text-3xl font-bold my-6">GitHub User Search</h1>
+      <Search />
     </div>
-    </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
